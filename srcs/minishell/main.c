1f4b04ed78ad8163beab662be1e7aec9b55fa781
Marius Van Pee <mariusvanpee@hotmail.be>
2024-02-12 19:12:19 +0100

    add static and test get env

--- srcs/minishell/main.c
+++ srcs/minishell/main.c
@@ -6,14 +6,14 @@
 /*   By: mvan-pee <mvan-pee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 21:37:59 by mvpee             #+#    #+#             */
-/*   Updated: 2024/02/12 19:04:11 by mvan-pee         ###   ########.fr       */
+/*   Updated: 2024/02/12 19:11:35 by mvan-pee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 #include <stdio.h>
 
-void	extract_int(char *env, t_env *env_vars, char **splitted_env)
+static void	extract_int(char *env, t_env *env_vars, char **splitted_env)
 {
 	if (!ft_strncmp(env, "SHLVL", 5))
 	{
@@ -23,7 +23,7 @@ void	extract_int(char *env, t_env *env_vars, char **splitted_env)
 	}	
 }
 
-void	extract_str(char *env, t_env *env_vars, char **splitted_env)
+static void	extract_str(char *env, t_env *env_vars, char **splitted_env)
 {
 	if (!ft_strncmp(env, "PWD", 3))
 	{
@@ -37,7 +37,7 @@ void	extract_str(char *env, t_env *env_vars, char **splitted_env)
 	}
 }
 
-t_env	ft_extract_env(char **env)
+static t_env	ft_extract_env(char **env)
 {
 	char	**splitted_env;
 	t_env	env_vars;
@@ -52,22 +52,24 @@ t_env	ft_extract_env(char **env)
 	return (env_vars);
 }
 
-int main(int ac, char **argv, char **env)
+static void env_init(t_env *env)
+{
+    env->path = NULL;
+    env->pwd = NULL;
+    env->shell_level = 0;
+}
+
+int main(int ac, char **argv, char **envs)
 {
     char *line = NULL;
-	
-	t_env test = ft_extract_env(env);
-	// int i = 0;
-	// while (test->path[i])
-	// {
-	// 	ft_printf("PATH %s\n", test->path[i]);
-	// 	i++;
-	// }
-	ft_printf("PWD %s\n", test.pwd);
-	// ft_printf("SHELL LEVEL %d\n", test->shell_level);
-	// while (*env)
-	// 	ft_printf("%s\n", *env++);
+	t_env env;
 
+    env_init(&env);
+    env = ft_extract_env(envs);
+
+    ft_printf("shlevel: %d\n", env.shell_level);
+    ft_printf("pwd: %s\n", env.pwd);
+    ft_printf("path0: %s\n", env.path[0]);
     while (777)
     {
         line = readline("minihell: ");

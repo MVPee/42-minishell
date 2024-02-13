727f349cb2c20ba4184310a82cc1a567e780a2c5
MVPee <mariusvanpee@hotmail.be>
2024-02-13 10:22:44 +0100

    norm for fun

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,34 +6,34 @@
 /*   By: mvpee <mvpee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 21:35:59 by mvpee             #+#    #+#             */
-/*   Updated: 2024/02/13 09:57:19 by mvpee            ###   ########.fr       */
+/*   Updated: 2024/02/13 10:22:26 by mvpee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#define RESET   "\x1b[0m"
-#define BOLD    "\x1b[1m"
-#define RED     "\x1b[31m"
-#define GREEN   "\x1b[32m"
-#define YELLOW  "\x1b[33m"
-#define BLUE    "\x1b[34m"
-#define MAGENTA "\x1b[35m"
-#define CYAN    "\x1b[36m"
-#define WHITE   "\x1b[37m"
+# define RESET "\x1b[0m"
+# define BOLD "\x1b[1m"
+# define RED "\x1b[31m"
+# define GREEN "\x1b[32m"
+# define YELLOW "\x1b[33m"
+# define BLUE "\x1b[34m"
+# define MAGENTA "\x1b[35m"
+# define CYAN "\x1b[36m"
+# define WHITE "\x1b[37m"
 
-#include "../srcs/libft/includes/libft.h"
-#include <readline/readline.h>
+# include "../srcs/libft/includes/libft.h"
+# include <readline/readline.h>
 
 // Buildins
-void ft_echo(char *str);
+void		ft_echo(char *str);
 
 typedef struct s_env
 {
 	int		shell_level;
 	char	*pwd;
 	char	**path;
-}	t_env;
+}			t_env;
 
 #endif

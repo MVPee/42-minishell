73957631f94da4b322a5b0d36fc2891996b1e8f8
Marius Van Pee <mariusvanpee@hotmail.be>
2024-02-14 14:40:25 +0100

    echo with 0

--- srcs/minishell/builtins/echo.c
+++ srcs/minishell/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: mvan-pee <mvan-pee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 17:46:20 by mvan-pee          #+#    #+#             */
-/*   Updated: 2024/02/14 10:57:28 by mvan-pee         ###   ########.fr       */
+/*   Updated: 2024/02/14 14:40:11 by mvan-pee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,31 @@
 
 void	ft_echo(t_data *data, char **split)
 {
-	int		i;
-	bool	flag;
+    int i = 0, j = 0;
+    bool flag;
 
-	flag = true;
-	i = 0;
-	while (split[++i])
-	{
-		if (i == 1 && ft_strncmp(split[i], "-n\0", 3) == 0)
-		{
-			i++;
-			flag = false;
-            if (!split[i])
-                return ;
+    flag = true;
+    if (!split[i] && !ft_strcmp(split[1], "-n"))
+    {
+        flag = false;
+        i++;
+    }
+    while(split[++i])
+    {
+        j = -1;
+        while(split[i][++j])
+        {
+            if (split[i][j] == '$' && split[i][j + 1] == '?')
+            {
+                ft_printf("%d", data->env_var);
+                j += 1;
+            }
+            else
+                ft_printf("%c", split[i][j]);
         }
-		if (split[i + 1] == NULL)
-			ft_printf("%s", split[i]);
-		else
-			ft_printf("%s ", split[i]);
-	}
-	if (flag)
-		ft_printf("\n");
-	data->env_var = 0;
+        if (split[i + 1] != NULL)
+ 		    ft_printf(" ");
+    }
+    if (flag)
+        ft_printf("\n");
 }

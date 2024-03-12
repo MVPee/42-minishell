fdcb7a33d29a3357a2013803ab766e3673b82259
Marius Van Pee <mariusvanpee@hotmail.be>
2024-03-12 15:14:49 +0100

    norm

--- srcs/minishell/lexer/lexer_utils.c
+++ srcs/minishell/lexer/lexer_utils.c
@@ -3,72 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   lexer_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mvpee <mvpee@student.42.fr>                +#+  +:+       +#+        */
+/*   By: mvan-pee <mvan-pee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 16:39:00 by mvpee             #+#    #+#             */
-/*   Updated: 2024/03/06 10:50:53 by mvpee            ###   ########.fr       */
+/*   Updated: 2024/03/12 15:11:09 by mvan-pee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
 
+static void get_quotes(char *line, int *i, char **buffer)
+{
+	if (line[*i] == '\"')
+	{
+		*buffer = ft_strjoinchar_free(buffer, line[*i]);
+		while (line[++(*i)] != '\"' && line[*i])
+			*buffer = ft_strjoinchar_free(buffer, line[*i]);
+	}
+	else if (line[*i] == '\'')
+	{
+		*buffer = ft_strjoinchar_free(buffer, line[*i]);
+		while (line[++(*i)] != '\'' && line[*i])
+			*buffer = ft_strjoinchar_free(buffer, line[*i]);
+	}
+}
+
 char	**get_cmd_splitted(char *line, int *count)
 {
 	char	**split;
-	char	buffer[1000];
+	char	*buffer;
 	int		i;
-	int		j;
 
 	split = NULL;
+	buffer = NULL;
 	i = -1;
-	j = 0;
 	while (line[++i])
 	{
-		if (line[i] == '\"')
+		get_quotes(line, &i, &buffer);
+		if (line[i] == '|')
 		{
-			buffer[j++] = line[i];
-			while (line[++i] != '\"' && line[i])
-			{
-				buffer[j] = line[i];
-				j++;
-			}
-			buffer[j++] = line[i];
-		}
-		else if (line[i] == '\'')
-		{
-			buffer[j++] = line[i];
-			while (line[++i] != '\'' && line[i])
-			{
-				buffer[j] = line[i];
-				j++;
-			}
-			buffer[j++] = line[i];
-		}
-		else if (line[i] == '|')
-		{
-			buffer[j] = '\0';
 			split = ft_splitjoin(split, buffer);
-			if (!split)
-				return (NULL);
-			(*count)++;
-			ft_memset(buffer, 0, 1000);
-			j = 0;
+			if (split)
+				(*count)++;
+			ft_free(1, &buffer);
 		}
 		else
-		{
-			buffer[j] = line[i];
-			j++;
-		}
-		if (!line[i])
-			break ;
+			buffer = ft_strjoinchar_free(&buffer, line[i]);
 	}
-	buffer[j] = '\0';
-	(*count)++;
 	split = ft_splitjoin(split, buffer);
-	if (!split)
-		return (NULL);
-	ft_memset(buffer, 0, 1000);
-	return (split);
+	if (split)
+		(*count)++;
+	return (ft_free(1, &buffer), split);
 }
 
 void	free_lexer(t_lexer *lexer)
